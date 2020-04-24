e9fe42ee37e55ee6574f5317e2a17e80bb102b0b
Luca Kanyo <lucakanyo@protonmail.com>
2020-04-24 23:40:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,7 +3,6 @@ def reverse_each_word_with_each(string)
   sentence = []
   words = string.split(' ')
   words.each do |word|
-    binding.pry
     sentence.push(word.reverse)
   end
   sentence.join(' ')
